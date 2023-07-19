f1aa868a0dc582954e03803ef8e718aaa34329a0
Gabozm09 <gzava004@fiu.edu>
2023-07-19 01:45:54 -0400

    Process Videos Locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+    
+    if(!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad request: Missing file path");
+    }
+    
+    ffmpeg(inputFilePath)
+     .outputOptions("-vf", "scale=-1:360")
+     .on("end", () => {
+        console.log('Processing finished successfully');
+        res.status(200).send("Processing finished successfully");
+     })
+     .on("error", (err) => {
+        console.log(`An error occurred: ${err.message}`);
+        res.status(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
+    
+    
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(
